056163258f2550631a58128a928c114c610e5ab9
bennhinnmwangi@gmail.com <bennhinnmwangi@gmail.com>
2026-01-16 14:43:27 +0300

    login page error fix

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import type { ReactNode } from 'react'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 
-export const dynamic = "force-dynamic";
+
 'use client'
 
 import { useState, useEffect } from 'react'
